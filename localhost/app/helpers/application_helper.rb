d07351b2a50b094a1f7e30ee0c4e2df5781566e5
samarv <samarvir1996@gmail.com>
2018-12-06 20:48:57 +0530

    add facebook login

--- localhost/app/helpers/application_helper.rb
+++ localhost/app/helpers/application_helper.rb
@@ -2,6 +2,10 @@ module ApplicationHelper
   def avatar_url(user)
     # gravatar_id = Digest::MD5::hexdigest(user.email).downcase
     # "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
-    "https://robohash.org/#{user.fullname}.jpg?size=150x150"     
+    if user.image
+      "http://graph.facebook.com/#{user.uid}/picture?type=large"
+    else
+      "https://robohash.org/#{user.fullname}.jpg?size=150x150"
+    end     
   end
 end
